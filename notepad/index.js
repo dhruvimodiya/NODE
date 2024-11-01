05903bd7f00d9a1bc4d05e2918b3019b1e2c8ae2
Dhruvi.Modiya <112712511+dhruvimodiya@users.noreply.github.com>
2024-11-01 16:39:17 +0530

    notepad

---
+++ notepad/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname,'public')));
+app.set('view engine','ejs');
+
+app.get('/',function(req,res){
+    // res.send("hello everyone");
+    res.render("index");
+})
+app.get('/profile/:username',function(req,res){ 
+    res.send(`welcome,${req.params.username}`);
+})
+app.get('/author/:username/:age',function(req,res){ 
+    res.send(`welcome,${req.params.username} of age ${req.params.age}`);
+})
+
+app.listen(3000,function(){
+    console.log("it's running");
+    
+})
