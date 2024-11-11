fc07bd3adc17e72e4a00a1a6335f1c259aa18f0c
Dhruvi.Modiya <112712511+dhruvimodiya@users.noreply.github.com>
2024-11-11 22:06:27 +0530

    Add files via upload

--- auth/app.js
+++ auth/app.js
@@ -1,59 +1,19 @@
 const cookieParser = require('cookie-parser');
 const express = require('express');
-const app = express();
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const app = express();
+const path = require('path');
 
+app.set("view engine",'ejs');
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname,'public')));
 app.use(cookieParser());
 
-// set cookie
-// app.get('/',function(req,res){
-//     // res.cookie('name',"dhruvi");
-//     res.send("done");
-// });
 
-// set password encrypt
-// app.get('/',function(req,res){
-//     // res.cookie('name',"dhruvi");
-//     bcrypt.genSalt(10,function(err,salt){
-//         bcrypt.hash("dhruvi",salt,function(err,hash){
-//             console.log(hash);  
-//         })
-//     })
-//     res.send("done");
-// });
-
-// for decrypt
-// app.get('/',function(req,res){
-//     bcrypt.compare("dhruvi","$2b$10$A.UVVL6hHUeMJLFozDr4DeenByydjcppLc0esDENJJrYrI5D7Z6.O",function(err,result){
-//         console.log(result);
-        
-//     })
-// });
-
-
-// jwt
 app.get('/',function(req,res){
-   let token = jwt.sign({email:"dhruvi123@gmail.com"},"secret")
-    // console.log("token -",token);
-    res.cookie("token",token);
-    res.send("done");
-    
-});
-
-app.get('/read',function(req,res){
-        console.log(req.cookies);
-        // res.send("read page done");
-        let data = jwt.verify(req.cookies.token,"secret")
-        console.log("data - ",data);
-        
-    });
-
-
-// read cookie
-// app.get('/read',function(req,res){
-//     // console.log(req.cookies);
-//     res.send("read page done");
-// });
+    res.render("index");
+})
 
 app.listen(4000);
